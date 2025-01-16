bdb9ab53120dc515bef687bf2910d9b1fddd5335
akshaywadatkar <akshay@truecover.net>
2025-01-16 21:32:15 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'sketch-board';
+
+  @ViewChild('canvas') canvasRef!: ElementRef<HTMLCanvasElement>;
+  private ctx!: CanvasRenderingContext2D;
+  private isDrawing = false;
+  currentColor = '#000000';
+  currentSize = 2;
+
+  ngAfterViewInit() {
+    const canvas = this.canvasRef.nativeElement;
+    this.ctx = canvas.getContext('2d')!;
+    this.resizeCanvas();
+  }
+
+  private resizeCanvas() {
+    const canvas = this.canvasRef.nativeElement;
+    canvas.width = window.innerWidth - 40;
+    canvas.height = window.innerHeight - 100;
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.resizeCanvas();
+  }
+
+  @HostListener('mousedown', ['$event'])
+  onMouseDown(event: MouseEvent) {
+    this.isDrawing = true;
+    this.draw(event);
+  }
+
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    if (this.isDrawing) {
+      this.draw(event);
+    }
+  }
+
+  @HostListener('mouseup')
+  onMouseUp() {
+    this.isDrawing = false;
+    this.ctx.beginPath();
+  }
+
+  onColorChange(event: Event) {
+    this.currentColor = (event.target as HTMLInputElement).value;
+  }
+
+
+  onSizeChange(event: Event) {
+    this.currentSize = parseInt((event.target as HTMLInputElement).value, 10);
+  }
+
+  private draw(event: MouseEvent) {
+    const rect = this.canvasRef.nativeElement.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    this.ctx.lineWidth = this.currentSize;
+    this.ctx.lineCap = 'round';
+    this.ctx.strokeStyle = this.currentColor;
+
+    this.ctx.lineTo(x, y);
+    this.ctx.stroke();
+    this.ctx.beginPath();
+    this.ctx.moveTo(x, y);
+  }
+  clearCanvas() {
+    const canvas = this.canvasRef.nativeElement;
+    this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
 }
